353e34d8f5c2adb4cf8d75826743b56e66251d0e
aryand10 <58601858+aryand10@users.noreply.github.com>
2024-05-05 23:34:20 -0700

    First Feature
    Correct Image and Source File Set once horn is selected

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,25 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const hornSelect = document.getElementById('horn-select');
+  const hornImage = document.querySelector('img');
+  const audio = document.querySelector('audio');
+
+  hornSelect.addEventListener('change', function() {
+    // Update the image based on the selected horn
+    switch (hornSelect.value) {
+      case 'air-horn':
+        hornImage.src = 'assets/images/air-horn.svg';
+        audio.src = 'assets/audio/air-horn.mp3';
+        break;
+      case 'car-horn':
+        hornImage.src = 'assets/images/car-horn.svg';
+        audio.src = 'assets/audio/car-horn.mp3';
+        break;
+      case 'party-horn':
+        hornImage.src = 'assets/images/party-horn.svg';
+        audio.src = 'assets/audio/party-horn.mp3';
+        break;
+    }
+  });
 }
